0303b534d5b8483395531a28e9aa4927fa8494ef
Luiz Bonini <lc.bonini13@gmail.com>
2022-09-07 21:10:43 -0300

    Alura 1 modulo 7 aula 1

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Photo } from "./photo";
 
@@ -9,10 +9,17 @@ export class PhotoService {
 
   constructor(private http: HttpClient) {}
 
-  listFromUser(userName: string) {
+  // listFromUser(userName: string) {
 
+  //   return this.http
+  //       .get<Photo[]>(API + "/" + userName + "/photos");
+
+  // }
+
+  listFromUserPaginated(userName: string, page: number) {
+    const params = new HttpParams().append('page', page.toString());
     return this.http
-        .get<Photo[]>(API + "/" + userName + "/photos");
+        .get<Photo[]>(API + "/" + userName + "/photos", { params });
 
   }
 }
